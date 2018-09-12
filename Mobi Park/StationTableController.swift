e1736ce7d2880f83732ead9f7442e28dc53d8661
Nic Waller <code@nicwaller.com>
2018-09-12 00:43:15 -0700

    calculate distance to stations from a hardcoded origin point

--- Mobi Park/StationTableController.swift
+++ Mobi Park/StationTableController.swift
@@ -7,13 +7,28 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
 class StationTableController: UIViewController, UITableViewDataSource {
-
+    
     @IBOutlet weak var stationTableView: UITableView!
     
+//    Raw data downloaded from API
     var stationList: [[String: Any]] = []
-
+    
+//    Clean, structured, ordered data refreshed periodically
+    var nearbyStationList: [NearbyStation] = []
+    struct NearbyStation {
+        var name: String
+        var location: CLLocation
+        var distance: CLLocationDistance // recalculated from location
+        var total_slots: Int
+        var free_slots: Int
+        var available_bikes: Int
+        var operative: Bool
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         loadStations()
@@ -25,6 +40,18 @@ class StationTableController: UIViewController, UITableViewDataSource {
         // Dispose of any resources that can be recreated.
     }
     
+    let f: MKDistanceFormatter = MKDistanceFormatter()
+    
+    func recalculateDistances() {
+        let myLocation: CLLocation = CLLocation(latitude: 49.28292, longitude: -123.1154539)
+        //        what is my current location actually? TODO: remove this
+        
+        for (var element) in nearbyStationList {
+            element.distance = element.location.distance(from: myLocation)
+        }
+        nearbyStationList.sort(by: { $0.distance < $1.distance })
+    }
+    
     func loadStations() {
         let url = URL(string: "https://vancouver-ca.smoove.pro/api-public/stations")!
         let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
@@ -33,7 +60,30 @@ class StationTableController: UIViewController, UITableViewDataSource {
             do {
                 let jsonResponse = try JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 if let result = jsonResponse["result"] as? [[String: Any]] {
-                    self.stationList = result
+//                    self.stationList = result
+                    let downtown: CLLocation = CLLocation(latitude: 49.28292, longitude: -123.1154539)
+                    for (element) in result {
+                        let name: String = element["name"] as! String
+                        let coordinates: String = element["coordinates"] as! String
+                        let coord_parts: [String] = coordinates.components(separatedBy: ",")
+                        let latitude: Double? = Double(coord_parts[0].trimmingCharacters(in: .whitespacesAndNewlines))
+                        let longitude: Double? = Double(coord_parts[1].trimmingCharacters(in: .whitespacesAndNewlines))
+                        let location: CLLocation = CLLocation(latitude: latitude!, longitude: longitude!)
+                        let distance: CLLocationDistance = location.distance(from: downtown)
+                        let total_slots: Int = element["total_slots"] as! Int
+                        let free_slots: Int = element["free_slots"] as! Int
+                        let available_bikes: Int = element["avl_bikes"] as! Int
+                        let operative: Bool = element["operative"] as! Bool
+
+                        let station = NearbyStation(name: name, location: location, distance: distance, total_slots: total_slots, free_slots: free_slots, available_bikes: available_bikes, operative: operative)
+                        
+                        // If the station is more than 1 km away, that's too far
+                        if distance > CLLocationDistance(1000) {
+                            continue
+                        }
+                        self.nearbyStationList.append(station)
+                    }
+                    self.recalculateDistances()
                     DispatchQueue.main.async {
                         self.stationTableView.reloadData()
                     }
@@ -58,17 +108,13 @@ class StationTableController: UIViewController, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return stationList.count
+        return nearbyStationList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = stationTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        if let station: [String: Any] = stationList[indexPath.row] {
-//            print(station)
-            cell.textLabel?.text = station["name"] as? String
-        } else {
-            cell.textLabel?.text = String(indexPath.row) + " Error"
-        }
+        let station = nearbyStationList[indexPath.row]
+        cell.textLabel?.text = station.name + " -- " + f.string(fromDistance: station.distance)
         return cell
     }
 
