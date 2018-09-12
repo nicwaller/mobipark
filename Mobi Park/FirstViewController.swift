67422d063ea2d1e876d33c8d34ffd8fd26053603
Nic Waller <code@nicwaller.com>
2018-09-11 22:06:04 -0700

    Xcode boilerplate: Tabbed App

---
+++ Mobi Park/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Mobi Park
+//
+//  Created by Nic Waller on 2018-09-11.
+//  Copyright © 2018 Nic Waller. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
