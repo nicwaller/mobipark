8f77cea5e8f4f351d4f9163c77f862934fae007f
Nic Waller <code@nicwaller.com>
2018-09-11 22:53:07 -0700

    add URL fetch though I'm not yet sure how to convert JSON text into a structured object in Swift

--- Mobi Park/StationTableController.swift
+++ Mobi Park/StationTableController.swift
@@ -14,12 +14,21 @@ class StationTableController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        loadStations()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func loadStations() {
+        let url = URL(string: "https://vancouver-ca.smoove.pro/api-public/stations")!
+        let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+            guard let data = data else { return }
+            print(String(data: data, encoding: .utf8)!)
+        }
+        task.resume()
     }
 
 
